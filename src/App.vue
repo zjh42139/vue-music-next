2ef787282e5835cb5eb46b62fa7413874c159c92
zjh42139 <1095699075@qq.com>
2022-07-10 03:15:34 +0800

    5-1

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <m-header></m-header>
   <tab></tab>
   <router-view></router-view>
+  <player></player>
 </template>
 
 <script setup>
 import MHeader from "@/components/header/header";
 import Tab from "@/components/tab/tab";
+import player from "./components/player/player";
 </script>
 
 <style lang="scss"></style>
